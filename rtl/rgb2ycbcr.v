5234eee3fd13159eb89354b67eea6f3d4c9febfa
JIN <xj19930618@gmail.com>
2023-02-02 13:59:17 +0100

    finish

--- rtl/rgb2ycbcr.v
+++ rtl/rgb2ycbcr.v
@@ -4,16 +4,16 @@ module rgb2ycbcr (
     input   wire                sys_rst_n           ,   //reset signal with low level valid
 
     // RGB image input port
-    input   wire                rgb_hsync           ,   //Horizontal Synchronization signal for input image
-    input   wire                rgb_vsync           ,   //Vertikale Synchronization signal for input image
-    input   wire    [15:0]      pix_data_in         ,   //rgb pix data
-    input   wire                image_data_valid    ,   
+    input   wire                per_frame_href      ,   //Horizontal Synchronization signal for input image
+    input   wire                per_frame_vsync     ,   //Vertikale Synchronization signal for input image
+    input   wire    [23:0]      pix_data_in         ,   //rgb pix data
+    input   wire                per_frame_clken     ,   
 
     // YCbCr output port   
     output  wire    [23:0]      gray_data           ,   //YCbCr pix data
-    output  reg                 ycbcr_valid         ,   
-    output  reg                 ycbcr_vsync         ,   //Horizontal Synchronization signal for output image
-    output  reg                 ycbcr_hsync             //Vertikale Synchronization signal for output image
+    output  wire                post_frame_clken    ,   
+    output  wire                post_frame_vsync    ,   //Vertikale Synchronization signal for output image
+    output  wire                post_frame_href        //Horizontal Synchronization signal for output image   
  
 );
 
@@ -44,23 +44,21 @@ reg  [7:0] Y_2  ;
 reg  [7:0] Cb_2 ;
 reg  [7:0] Cr_2 ; 
 
-reg  rgb_hsync_ack;
-reg  rgb_hsync_ack_1;
 
-reg rgb_vsync_ack;
-reg rgb_vsync_ack_1;
 
-reg image_data_valid_ack;
-reg image_data_valid_ack_1;
+
 
 //*******************************************************************************************************************//
 //******************************************************main code****************************************************//
 //*******************************************************************************************************************//
 
 //pix data with form "RGB565" to "RGB888"
-assign R_0 = {pix_data_in [15:11] , pix_data_in [15:13]} ;
-assign G_0 = {pix_data_in [10:5] , pix_data_in [10:9]};
-assign B_0 = {pix_data_in [4:0] , pix_data_in [4:2]};
+//assign R_0 = {pix_data_in [15:11] , pix_data_in [15:13]} ;
+//assign G_0 = {pix_data_in [10:5] , pix_data_in [10:9]};
+//assign B_0 = {pix_data_in [4:0] , pix_data_in [4:2]};
+assign R_0 = pix_data_in [23:16]    ;
+assign G_0 = pix_data_in [15:8]     ;
+assign B_0 = pix_data_in [7:0]      ;
 
 //frist level operation: multiplication operation
 always@(posedge sys_clk or negedge sys_rst_n)
@@ -73,9 +71,9 @@ always@(posedge sys_clk or negedge sys_rst_n)
             end
         else 
             begin
-                {R_1, G_1, B_1} <= { {R0 * 16'd77} , {G0 * 16'd150} , {B0 * 16'd29} }; 
-                {R_2, G_2, B_2} <= { {R0 * 16'd43} , {G0 * 16'd85} , {B0 * 16'd128} };
-                {R_3, G_3, B_3} <= { {R0 * 16'd128}, {G0 * 16'd107} , {B0 * 16'd21} };
+                {R_1, G_1, B_1} <= { {R_0 * 16'd77} , {G_0 * 16'd150} , {B_0 * 16'd29} }; 
+                {R_2, G_2, B_2} <= { {R_0 * 16'd43} , {G_0 * 16'd85} ,  {B_0 * 16'd128} };
+                {R_3, G_3, B_3} <= { {R_0 * 16'd128}, {G_0 * 16'd107} , {B_0 * 16'd21} };
             end
     end
 
@@ -113,53 +111,28 @@ always@(posedge sys_clk or negedge sys_rst_n)
             end
     end
 
-assign gray_data = {Y_2 [7:0], Y_2 [7:0], Y_2 [7:0]};
-
+reg	[2:0]	per_frame_vsync_r;
+reg	[2:0]	per_frame_href_r;	
+reg	[2:0]	per_frame_clken_r;
 always@(posedge sys_clk or negedge sys_rst_n)
-    begin
-        if (sys_rst_n == 1'b0)
-            begin
-                rgb_hsync_ack   <= 1'b0;
-                rgb_hsync_ack_1 <= 1'b0;
-                ycbcr_hsync     <= 1'b0;
-            end
-        else
-            begin
-                rgb_hsync_ack   <= rgb_hsync;
-                rgb_hsync_ack_1 <= rgb_hsync_ack;
-                ycbcr_hsync     <= ycbcr_hsync;
-            end  
-    end
+begin
+	if(sys_rst_n == 1'b0)
+		begin
+		per_frame_vsync_r <= 0;
+		per_frame_href_r <= 0;
+		per_frame_clken_r <= 0;
+		end
+	else
+		begin
+		per_frame_vsync_r 	<= 	{per_frame_vsync_r[1:0], 	per_frame_vsync};
+		per_frame_href_r 	<= 	{per_frame_href_r[1:0], 	per_frame_href};
+		per_frame_clken_r 	<= 	{per_frame_clken_r[1:0], 	per_frame_clken};
+		end
+end
+assign	post_frame_vsync 	= 	per_frame_vsync_r[2];
+assign	post_frame_href 	= 	per_frame_href_r[2];
+assign	post_frame_clken 	= 	per_frame_clken_r[2];
 
-always@(posedge sys_clk or negedge sys_rst_n)
-    begin
-        if (sys_rst_n == 1'b0)
-            begin
-                rgb_vsync_ack   <= 1'b0;
-                rgb_vsync_ack_1 <= 1'b0;
-                ycbcr_vsync     <= 1'b0;
-            end
-        else
-            begin
-                rgb_vsync_ack   <= rgb_vsync;
-                rgb_vsync_ack_1 <= rgb_vsync_ack;
-                ycbcr_vsync     <= ycbcr_vsync;
-            end  
-    end
+assign  gray_data = (post_frame_href == 1'b1) ? {Y_2 [7:0], Y_2 [7:0], Y_2 [7:0]} : 24'd0;
 
-always@(posedge sys_clk or negedge sys_rst_n)
-    begin
-        if (sys_rst_n == 1'b0)
-            begin
-                image_data_valid_ack   <= 1'b0;
-                image_data_valid_ack_1 <= 1'b0;
-                ycbcr_valid            <= 1'b0;
-            end
-        else
-            begin
-                image_data_valid_ack   <= image_data_valid;
-                image_data_valid_ack_1 <= image_data_valid_ack;
-                ycbcr_valid            <= image_data_valid_ack_1;
-            end  
-    end
 endmodule
